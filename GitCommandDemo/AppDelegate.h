6af22aa4216a0fef4b762a6d833de56815538771
liquangang <liquangang@idstaff.com>
2017-06-13 16:58:04 +0800

    init proj

---
+++ GitCommandDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitCommandDemo
+//
+//  Created by quangang on 2017/6/13.
+//  Copyright © 2017年 LQG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitCommandDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitCommandDemo
+//
+//  Created by quangang on 2017/6/13.
+//  Copyright © 2017年 LQG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
